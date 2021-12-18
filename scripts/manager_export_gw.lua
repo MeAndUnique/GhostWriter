e73bf7f943467d715af7dc3f539e847183878a14
Shane Parker <shane.tim.parker@ea.com>
2021-12-18 16:02:45 -0500

    Added per-type player default selection

--- scripts/manager_export_gw.lua
+++ scripts/manager_export_gw.lua
@@ -7,6 +7,7 @@ local addExportNodeOriginal;
 local performExportOriginal;
 
 local bPlayerVisible;
+local recordTypePlayerDefault = {};
 local nodeChaptersBackup = nil;
 local removedPages = {};
 
@@ -22,13 +23,16 @@ end
 
 function performExport(wExport)
 	bPlayerVisible = (wExport.playervisible.getValue() == 1);
+	for _,wRecordType in ipairs(wExport.recordtypes.getWindows()) do
+		recordTypePlayerDefault[wRecordType.getExportType()] = wRecordType.playerdefault.getValue();
+	end
 	performExportOriginal(wExport);
 end
 
 function addExportNode(nodeSource, sTargetPath, sExportType, sExportLabel, sExportListClass, sExportRootPath)
 	if ReferenceManualManager.MANUAL_DEFAULT_INDEX .. "." .. ReferenceManualManager.MANUAL_DEFAULT_CHAPTER_LIST_NAME == sTargetPath then
 		handleChaptersNode(nodeSource);
-	elseif not checkForExport(nodeSource) then
+	elseif not checkForExport(nodeSource, sExportType) then
 		if sExportType == "referencemanualpage" then
 			removedPages[nodeSource.getPath()] = true;
 		end
@@ -38,10 +42,14 @@ function addExportNode(nodeSource, sTargetPath, sExportType, sExportLabel, sExpo
 	addExportNodeOriginal(nodeSource, sTargetPath, sExportType, sExportLabel, sExportListClass, sExportRootPath);
 end
 
-function checkForExport(nodeSource)
+function checkForExport(nodeSource, sExportType)
 	local sExport = DB.getValue(nodeSource, "exportcontrol");
 	if (sExport or "") == "" then
-		sExport = OptionsManager.getOption("GWDE")
+		if recordTypePlayerDefault[sExportType] then
+			sExport = "player";
+		else
+			sExport = "gm";
+		end
 	end
 	return (sExport == "both") or (bPlayerVisible == (sExport == "player"));
 end
